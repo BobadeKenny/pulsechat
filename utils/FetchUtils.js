586beb855679b877789e52b6bd0a5f300f0c2bce
Kehinde Bobade <kehinde.bobade@mpharma.com>
2025-12-03 15:01:39 +0100

    refactor customfetch

--- utils/FetchUtils.js
+++ utils/FetchUtils.js
@@ -1,5 +1,7 @@
 import * as SecureStore from "expo-secure-store";
 
+const BASE_API_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
+
 async function saveTokens(tokens) {
   if (tokens.access) {
     await SecureStore.setItemAsync("access", tokens.access);
@@ -14,13 +16,10 @@ async function deleteTokens() {
   await SecureStore.deleteItemAsync("refresh");
 }
 
-async function customFetch(url, method = "GET", body = null) {
-  const BASE_API_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
-  try {
-    const refreshToken = await SecureStore.getItemAsync("refresh");
-
-    if (refreshToken != null) {
-
+async function refreshAccessToken() {
+  const refreshToken = await SecureStore.getItemAsync("refresh");
+  if (refreshToken) {
+    try {
       const response = await fetch(`${BASE_API_URL}auth/token/refresh/`, {
         method: "POST",
         headers: {
@@ -39,28 +38,40 @@ async function customFetch(url, method = "GET", body = null) {
 
       const tokens = await response.json();
       await saveTokens(tokens);
-
-      const accessToken = tokens.access;
-      const resp = await fetch(`${BASE_API_URL}${url}`, {
-        method: method,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: body ? JSON.stringify(body) : null,
-      });
-      if (!resp.ok) {
-        error = await resp.json();
-        throw new Error(error);
-      }
-      const data = await resp.json();
-      return data;
-    } else {
+      return tokens.access;
+    } catch (error) {
+      console.log("Error refreshing access token: ", error);
       return null;
     }
+  } else {
+    return null;
+  }
+}
+
+async function customFetch(url, method = "GET", body = null) {
+  try {
+    const accessToken = await refreshAccessToken();
+    if (!accessToken) {
+      throw new Error("No access token available");
+    }
+    const resp = await fetch(`${BASE_API_URL}${url}`, {
+      method: method,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: body ? JSON.stringify(body) : null,
+    });
+    if (!resp.ok) {
+      error = await resp.json();
+      throw new Error(error);
+    }
+    const data = await resp.json();
+    return data;
   } catch (error) {
-    console.log(error);
+    console.log("Fetch error: ", error);
+    throw error;
   }
 }
 
